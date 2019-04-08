3e94829b6574d6fcd6241461d7974d6a2d2f9996
madiyar99 <35283030+madiyar99@users.noreply.github.com>
2019-04-09 02:46:17 +0600

    front and back

--- week11/to-do front/toDoFront/src/app/components/main/main.component.ts
+++ week11/to-do front/toDoFront/src/app/components/main/main.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('counting') counter;
+  i:any;
+
+  @ViewChild('formInput') note;  //note_text
+  @ViewChild('contBlock') block;
+ 
+  @ViewChild('noteBlock') divInit;
+  divCurr:any;
+  par:any;
+  checkBox:any;
+
+  constructor() { 
+
+  }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {}
+
+  CreateDiv() {
+  	
+  	this.i = this.counter.nativeElement.innerHtml; //Counter of id's
+  	var a = this.divInit.nativeElement.cloneNode(true);
+  	a.nativeElement.style.display = "block";
+
+  	this.par = this.divCurr.nativeElement.firstChild.children[1];
+  	this.par.nativeElement.innerHtml = this.note.nativeElement.value;
+  	this.par.nativeElement.id = this.par.nativeElement.id + this.i;
+
+  	this.checkBox = this.divCurr.nativeElement.firstChild.firstChild;
+  	this.checkBox.nativeElement.id = this.i;
+
+  	this.divCurr.nativeElement.id = this.divCurr.nativeElement.id + 1;
+  	this.block.nativeElement.appendChild(this.divCurr);
+
+  	this.i = Number(this.i);
+  	this.counter.nativeElement.innerHtml = this.i + 1;
   }
 
 
