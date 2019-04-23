2739d3d1cdc16c487fc59a724c657f27cd6dfd81
madiyar99 <35283030+madiyar99@users.noreply.github.com>
2019-04-23 13:24:16 +0600

    added

--- week11/to-do back/mysite/mysite/settings.py
+++ week11/to-do back/mysite/mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'webexample',
     'shop',
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- week11/to-do back/mysite/mysite/urls.py
+++ week11/to-do back/mysite/mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('webexample/', include('webexample.urls')),
     path('shop/', include('shop.urls')),
+    path('api/', include('api.urls'))
 ]
